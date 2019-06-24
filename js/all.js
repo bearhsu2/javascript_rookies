60e7b724de06e480f5bf3dbe406fe42ac42ad3de
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-24 22:43:39 +0800

    calculation

--- js/all.js
+++ js/all.js
@@ -1,6 +1,11 @@
 var cookiePrice = 35;
+var myMoney = 15;
 
 
-document.getElementById("price").textContent = cookiePrice;
+
+
+document.getElementById("cookiePrice").textContent = cookiePrice;
+document.getElementById("myMoney").textContent = myMoney;
+document.getElementById("diff").textContent = cookiePrice - myMoney;
 
 console.log(cookiePrice);
