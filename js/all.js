dbd3bbe95faca75af409dd2f2241970387e9e67e
Yu-Song Syu <kuma@simbo.com.tw>
2019-06-26 23:14:02 +0800

    function def

--- js/all.js
+++ js/all.js
@@ -2,10 +2,27 @@ var cookiePrice = 35;
 var myMoney = 15;
 
 
-
-
 document.getElementById("cookiePrice").textContent = cookiePrice;
 document.getElementById("myMoney").textContent = myMoney;
 document.getElementById("diff").textContent = cookiePrice - myMoney;
 
 console.log(cookiePrice);
+
+
+function greet() {
+    console.log('Hello');
+    console.log('World')
+}
+
+function count(a, b) {
+    console.log('total = ' + a*b);
+}
+
+greet();
+count(3,4);
+
+function show() {
+    console.log(myMoney);
+
+}
+show();
