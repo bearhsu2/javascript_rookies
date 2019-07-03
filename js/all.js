0959cccb5c12f5bd3202e7eca57232f6963b37c1
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-03 22:25:00 +0800

    hehe

--- js/all.js
+++ js/all.js
@@ -5,7 +5,25 @@ document.getElementById('countId').onclick = function () {
     var hamNum = parseInt(document.getElementById('hamNumId').value);
     var cokeNum = parseInt(document.getElementById('cokeNumId').value);
 
+
     document.getElementById('totalId').textContent = hamNum * hamPrice + cokeNum * cokePrice;
 
 
 }
+
+var cornField = [8, 5, 6];
+console.log(cornField);
+
+
+var farm = {
+    farmer: 'casper',
+    dogs: ['james', 'bond'],
+
+    cornField: [8, 5, 6]
+};
+
+farm.chick = 15;
+
+console.log(farm);
+console.log(farm.chick);
+console.log(farm.dogs[7]);
