01774a58025ba629ba5500cba0e27aa7d76cb374
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-10 15:19:39 +0800

    birthday example

--- js/all.js
+++ js/all.js
@@ -1,34 +1,12 @@
-document.getElementById('countId').onclick = function () {
+var thisMonth = 7;
+var myMonth = 11;
 
-    var hamPrice = 50;
-    var cokePrice = 20;
-    var hamNum = parseInt(document.getElementById('hamNumId').value);
-    var cokeNum = parseInt(document.getElementById('cokeNumId').value);
-
-
-    document.getElementById('totalId').textContent = hamNum * hamPrice + cokeNum * cokePrice;
+var isBirthday = thisMonth == myMonth;
+document.getElementById('birthdayId').textContent = isBirthday;
 
 
-}
+var totalPeople = 2;
+var nowPeople = 1;
 
-var cornField = [8, 5, 6];
-console.log(cornField);
-
-
-var farm = {
-    farmer: 'casper',
-    dogs: ['james', 'bond'],
-    cornField: [8, 5, 6],
-    goDinner: function () {
-        console.log(farm.farmer + ', go home for dinner!!!');
-    }
-};
-
-
-var farms = [
-    {farmer: 'casper'},
-    {farmer: 'peter'}
-]
-
-console.log(farms[0].farmer);
-console.log(farms[1].farmer);
+var notAllArrived = totalPeople != nowPeople;
+document.getElementById('peopleId').textContent = notAllArrived;
