76dac527cfc8e5c268628e5cba98627ff9512056
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-15 16:54:51 +0800

    createElement + for

--- js/all.js
+++ js/all.js
@@ -1,9 +1,18 @@
-var str = document.createElement('em');
-str.textContent = '1234';
-str.setAttribute('class', 'blue');
+var farms = [
+    {
+        farmer: "Bob",
+        yeye: 45
+    }, {
+        farmer: "Martin",
+        nono: 22
+    }
+];
 
+var len = farms.length;
 
-var el = document.querySelector('.title');
-el.appendChild(str);
-
-
+var el = document.querySelector(".list");
+for(var i = 0; i < len; i++){
+    var str = document.createElement('li');
+    str.textContent = farms[i].farmer;
+    el.appendChild(str);
+}
