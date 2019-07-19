f9b35d270a97b53eef2711a1493170817f29af1b
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-19 22:35:32 +0800

    e.target

--- js/all.js
+++ js/all.js
@@ -1,6 +1,7 @@
-var element = document.querySelector('.link');
+var element = document.querySelector('.header');
 
-element.addEventListener('click', function (event) {
-    alert("WOW!!!")
-    event.preventDefault();
+element.addEventListener('click', function (e) {
+    console.log(e.target);
+    console.log(e.target.nodeName);
+    console.log(e.target.nodeType);
 });
