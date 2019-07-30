6f720d11c0227b4cf5589e3570f7cd48c2a503fb
Yu-Song Syu <kuma@simbo.com.tw>
2019-07-30 23:30:36 +0800

    can handle null

--- js/all.js
+++ js/all.js
@@ -13,19 +13,34 @@ function putAndShow() {
     todoList.push(inputTextValue);
 
     window.localStorage.setItem('todoList', JSON.stringify(todoList));
+
+    updateList();
 }
 
 inputButton.addEventListener('click', putAndShow);
 
 
+var list = document.querySelector('.list');
 
-// var list  = document.querySelector('.list');
-//
-//
-// function updateList(event) {
-//
-//
-//
-// }
-//
-// item.addEventListener('click', updateList, false);
+
+function updateList(event) {
+
+    var todoListString = window.localStorage.getItem('todoList');
+    if (todoListString === null || todoListString == '') {
+        todoListString = '[]';
+    }
+    todoList = JSON.parse(todoListString);
+
+    var length = todoList.length;
+    var string = '';
+    for (var i = 0; i < length; i++) {
+
+        string += "<li data-num='" + i + "'>" + todoList[i] + "</li>";
+
+    }
+
+    list.innerHTML = string;
+
+}
+
+updateList();
