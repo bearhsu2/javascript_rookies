68920493b799dfa60c89e1c14fe989e60def9935
Yu-Song Syu <kuma@simbo.com.tw>
2019-10-25 11:57:15 +0800

    logon and login

--- js/all.js
+++ js/all.js
@@ -1,6 +1,5 @@
-var send = document.querySelector('.send');
-
-send.addEventListener('click', signup, false);
+document.querySelector('.signup').addEventListener('click', signup, false);
+document.querySelector('.login').addEventListener('click', login, false);
 
 function signup() {
 
@@ -15,14 +14,40 @@ function signup() {
     var accountStr = JSON.stringify(account);
 
     var xhr = new XMLHttpRequest();
-    xhr.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signup',true);
-    xhr.setRequestHeader('Content-type','application/json');
+    xhr.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signup', true);
+    xhr.setRequestHeader('Content-type', 'application/json');
     xhr.send(accountStr);
     xhr.onload = function () {
+        var response = JSON.parse(xhr.responseText);
 
+        alert(response.message);
+    }
+
+
+}
+
+function login() {
+
+    var email = document.querySelector('.email').value;
+    var password = document.querySelector('.password').value;
+
+    var account = {
+        email: email,
+        password: password
+    }
+
+    var accountStr = JSON.stringify(account);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', 'https://hexschool-tutorial.herokuapp.com/api/signin', true);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.send(accountStr);
+    xhr.onload = function () {
         var response = JSON.parse(xhr.responseText);
 
         alert(response.message);
     }
 
 }
+
+
