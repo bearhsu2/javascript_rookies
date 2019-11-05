9b2a700ca9e450bd95c0d152e02cbe86b2e8312b
Yu-Song Syu <kuma@simbo.com.tw>
2019-11-05 23:53:38 +0800

    markdown parser done

--- js/all.js
+++ js/all.js
@@ -1,37 +1,19 @@
-var apiUrlPrefix = 'https://api.github.com/repos/vuejs/vue/commits?per_page=3&sha=';
 var app = new Vue({
 
     el: "#app",
     data: {
-        branches: ['master', 'dev'],
-        currentBranch: 'master',
-        commits: null
-    },
-    created: function () {
-        this.fetchData()
-    },
-    watch: {
-        currentBranch: function () {
-            this.fetchData();
-        }
+        inputText: '# hello'
     },
     methods: {
-        fetchData: function () {
-            const xhr = new XMLHttpRequest();
-            const vm = this;
+        update: _.debounce(function (e) {
+            this.inputText = e.target.value;
+        }, 300),
 
-            xhr.open('GET', apiUrlPrefix + vm.currentBranch);
-            xhr.onload = function(){
-                vm.commits = JSON.parse(xhr.responseText);
-                console.log(vm.commits);
-
-            };
-            xhr.send();
-
-
-            // alert(apiUrlPrefix);
-        }
     },
-    computed: {}
+    computed: {
+        markedText: function () {
+            return marked(this.inputText);
+        }
+    }
 
 });
